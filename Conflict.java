fe3e27f3d09eb4dd309b8407431583627a76e3dc
ryosei ono <r.ono1841@morijyobi.ac.jp>
2019-06-04 11:55:17 +0900

    [modify]Conflict.javaを修正

--- Conflict.java
+++ Conflict.java
@@ -1,5 +1,5 @@
-public class{
-	public static void Main(String[] args){
+public class Conflict{
+	public static void main(String[] args){
 for(int i=0;i<10;i++){system.out.println(java.util.Random().nextInt(100)+1);}
 	}
 }
